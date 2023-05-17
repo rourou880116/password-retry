763a141261fa436ba737da9acd704900b24912b4
rourou <rourou880116@gmail.com>
2023-05-17 13:31:04 +0800

    change to while i > 0

--- pwd.py
+++ pwd.py
@@ -1,15 +1,11 @@
 password = "a123456"
-x = 1
-while True:
+x = 3
+while x > 0:
 	pwd = input('請輸入密碼: ')
 	if pwd == password:
 		print('登入成功')
 		break
 	else:
-		while x < 4:
-			print('密碼錯誤 剩下可嘗試次數: ', 3 - x )
-			x = x + 1
-			break
-	if x == 4:
-		print('無法再登入')
-		break
+		x = x - 1
+		print('密碼錯誤! 還有', x,'次機會')
+
